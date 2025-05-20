adf368dc9b3835fda433d0c1f5ce072005706618
Abdullah Fatota <53046436+new-AF@users.noreply.github.com>
2025-05-20 15:08:01 +0200

    add navbar

---
+++ src/Navbar.jsx
@@ -0,0 +1,26 @@
+export const Navbar = ({ title }) => {
+    return (
+        <div className="daisy-navbar bg-base-100 shadow-sm">
+            <div className="flex-1">
+                <a className="daisy-btn daisy-btn-ghost text-xl">{title}</a>
+            </div>
+            <div className="flex-none">
+                <button className="daisy-btn daisy-btn-square daisy-btn-ghost">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        className="inline-block h-5 w-5 stroke-current"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+                        ></path>{" "}
+                    </svg>
+                </button>
+            </div>
+        </div>
+    );
+};
