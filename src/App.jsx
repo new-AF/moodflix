becfc1165f13fd62a594e58bcb8c118f0b006ff7
Abdullah Fatota <53046436+new-AF@users.noreply.github.com>
2025-05-20 18:23:19 +0200

    add api key (.env file ignored)

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import { Navbar } from "./Navbar";
 function App() {
     const [count, setCount] = useState(0);
 
+    const apiKey = import.meta.env.VITE_OMDB_API_KEY;
+
+    console.log(apiKey); // API key
+
     return (
         <>
             <Navbar title={"My Moodflix"} />
